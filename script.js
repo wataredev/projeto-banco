5a11f4dea17fab5f892ea79bab3c95bbd8a0b0e6
Rodrigofms <rodrigoms.rec@gmail.com>
2024-09-01 17:18:17 -0300

    feat: login e novas funcionalidades

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 function entrar() {
   let senha = parseInt(document.querySelector("#input-senha").value);
+  let usuario = document.querySelector("#input-iniciar").value;
 
-  if (senha == 3589) {
+
+  if (senha == 3589 && usuario.length !== 0 && usuario.length <= 20 && !/[^A-Za-z\sáàâãéèêíïóôõöúçñÁÀÂÃÉÈÊÍÏÓÔÕÖÚÇÑ]/.test(usuario)) {
+    localStorage.setItem("usuario", usuario);
     window.open("banco.html", "_self");
+  } else if (usuario.length === 0 || usuario.length > 20 || /[^A-Za-z\sáàâãéèêíïóôõöúçñÁÀÂÃÉÈÊÍÏÓÔÕÖÚÇÑ]/.test(usuario)) {
+    alert("Usuario inválido! Nome maior que 15 caracteres e/ou vazio e\ou com caracteres especiais");
   } else {
-    alert("Senha inválida! a senha correta é 3589")``
+    alert("Senha inválida! a senha correta é 3589");
   }
 }
-
