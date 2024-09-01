ba9efd8b00e10c49d8392d7f23a5b3bb3888657c
Rodrigofms <rodrigoms.rec@gmail.com>
2024-09-01 16:21:17 -0300

    fix: redirecionamento por botao na pagina inicial

--- banco.js
+++ banco.js
@@ -1,81 +1,81 @@
 
-var dinheiroTotal = 5000;
+let dinheiroTotal = 5000;
 
 function atualizarSaldo(valor) {
-    let textarea = document.querySelector("#valorDisponivel");
-    textarea.placeholder = `Saldo: ${valor}`;
+  let textarea = document.querySelector("#valorDisponivel");
+  textarea.placeholder = `Saldo: ${valor}`;
 }
 
 function verSaldo() {
-    esconder()
-    document.querySelector(".container-verSaldo").style.display = "flex";
+  esconder()
+  document.querySelector(".container-verSaldo").style.display = "flex";
 }
 
 function sacar() {
-    esconder()
-    document.querySelector(".container-sacar").style.display = "flex";
+  esconder()
+  document.querySelector(".container-sacar").style.display = "flex";
 }
 
 function verExtrato() {
-    esconder()
-    document.querySelector(".container-extrato").style.display = "flex";
+  esconder()
+  document.querySelector(".container-extrato").style.display = "flex";
 }
 
 function depositar() {
-    esconder()
-    document.querySelector(".container-depositar").style.display = "flex";
+  esconder()
+  document.querySelector(".container-depositar").style.display = "flex";
 }
 
 function transferir() {
-    esconder()
-    document.querySelector(".container-transferir").style.display = "flex";
+  esconder()
+  document.querySelector(".container-transferir").style.display = "flex";
 }
 
 
 
 function sacarDinheiro() {
-    let textarea = document.querySelector(".textSacar");
-    let valorDisponivel = parseFloat(textarea.value);
-    
-    if (isNaN(valorDisponivel) || !valorDisponivel) {
-        textarea.placeholder = "Valor inválido ou Vazio"
-    } else if (valorDisponivel <= dinheiroTotal) {
-        dinheiroTotal -= valorDisponivel;
-        atualizarSaldo(dinheiroTotal)
-        textarea.value = "";
-        textarea.placeholder = "Saque realizado"
-    } else {
-        textarea.value = "";
-        textarea.placeholder = "Saldo insuficiente";
-    }
+  let textarea = document.querySelector(".textSacar");
+  let valorDisponivel = parseFloat(textarea.value);
+
+  if (isNaN(valorDisponivel) || !valorDisponivel) {
+    textarea.placeholder = "Valor inválido ou Vazio"
+  } else if (valorDisponivel <= dinheiroTotal) {
+    dinheiroTotal -= valorDisponivel;
+    atualizarSaldo(dinheiroTotal)
+    textarea.value = "";
+    textarea.placeholder = "Saque realizado"
+  } else {
+    textarea.value = "";
+    textarea.placeholder = "Saldo insuficiente";
+  }
 }
 
 function depositarDinheiro() {
-    let textDepositar = document.querySelector(".textDepositar");
-    let valorDepositar = parseFloat(textDepositar.value);
-    console.log(valorDepositar);
+  let textDepositar = document.querySelector(".textDepositar");
+  let valorDepositar = parseFloat(textDepositar.value);
+  console.log(valorDepositar);
 
-    if (isNaN(valorDepositar) || !valorDepositar || valorDepositar < 0) {
-        textDepositar.placeholder = "Valor inválido ou Vazio"
-    } else {
-        dinheiroTotal += valorDepositar;
-        atualizarSaldo(dinheiroTotal);
-        textDepositar.value = "";
-        textDepositar.placeholder = "Deposito realizado"
-    }
+  if (isNaN(valorDepositar) || !valorDepositar || valorDepositar < 0) {
+    textDepositar.placeholder = "Valor inválido ou Vazio"
+  } else {
+    dinheiroTotal += valorDepositar;
+    atualizarSaldo(dinheiroTotal);
+    textDepositar.value = "";
+    textDepositar.placeholder = "Deposito realizado"
+  }
 }
 
 
 function esconder() {
-    document.querySelector(".container-verSaldo").style.display = "none";
-    document.querySelector(".container-sacar").style.display = "none";
-    document.querySelector(".container-extrato").style.display = "none";
-    document.querySelector(".container-depositar").style.display = "none";
-    document.querySelector(".container-transferir").style.display = "none";
-    document.querySelector(".container-inicial").style.display = "none";
+  document.querySelector(".container-verSaldo").style.display = "none";
+  document.querySelector(".container-sacar").style.display = "none";
+  document.querySelector(".container-extrato").style.display = "none";
+  document.querySelector(".container-depositar").style.display = "none";
+  document.querySelector(".container-transferir").style.display = "none";
+  document.querySelector(".container-inicial").style.display = "none";
 }
 
 function botaoVoltar() {
-    esconder()
-    document.querySelector(".container-inicial").style.display = "flex";
+  esconder()
+  document.querySelector(".container-inicial").style.display = "flex";
 }
--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-var usuario = document.querySelector("#input-iniciar").value;
-
-
 function entrar() {
-    let senha = parseInt(document.querySelector("#input-senha").value);
+  let senha = parseInt(document.querySelector("#input-senha").value);
 
-    if (senha == 3589) {
-        window.open("banco.html");
-    } else {
-        alert("Senha inválida! a senha correta é 3589")
-    }
+  if (senha == 3589) {
+    window.open("banco.html", "_self");
+  } else {
+    alert("Senha inválida! a senha correta é 3589")``
+  }
 }
 
