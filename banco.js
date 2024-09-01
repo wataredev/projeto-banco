b4fe6646c1f3b27d70eb3458702fcda8dbf721c5
Rodrigofms <rodrigoms.rec@gmail.com>
2024-09-01 17:18:17 -0300

    feat: login e novas funcionalidades

--- banco.js
+++ banco.js
@@ -1,6 +1,12 @@
 
 let dinheiroTotal = 5000;
 
+let nomeUsuario = localStorage.getItem("usuario")
+
+let textoBoasVindas = document.querySelector("h1").innerText = `Olá ${nomeUsuario}`;
+
+responsiveVoice.speak(textoBoasVindas, 'Brazilian Portuguese Female', { rate: 1.1 });
+
 function atualizarSaldo(valor) {
   let textarea = document.querySelector("#valorDisponivel");
   textarea.placeholder = `Saldo: ${valor}`;
@@ -39,14 +45,17 @@ function sacarDinheiro() {
 
   if (isNaN(valorDisponivel) || !valorDisponivel) {
     textarea.placeholder = "Valor inválido ou Vazio"
+    responsiveVoice.speak(textarea.placeholder, 'Brazilian Portuguese Female', { rate: 1.1 });
   } else if (valorDisponivel <= dinheiroTotal) {
     dinheiroTotal -= valorDisponivel;
     atualizarSaldo(dinheiroTotal)
     textarea.value = "";
     textarea.placeholder = "Saque realizado"
+    responsiveVoice.speak(textarea.placeholder, 'Brazilian Portuguese Female', { rate: 1.1 });
   } else {
     textarea.value = "";
     textarea.placeholder = "Saldo insuficiente";
+    responsiveVoice.speak(textarea.placeholder, 'Brazilian Portuguese Female', { rate: 1.1 });
   }
 }
 
@@ -57,11 +66,13 @@ function depositarDinheiro() {
 
   if (isNaN(valorDepositar) || !valorDepositar || valorDepositar < 0) {
     textDepositar.placeholder = "Valor inválido ou Vazio"
+    responsiveVoice.speak(textDepositar.placeholder, 'Brazilian Portuguese Female', { rate: 1.1 });
   } else {
     dinheiroTotal += valorDepositar;
     atualizarSaldo(dinheiroTotal);
     textDepositar.value = "";
     textDepositar.placeholder = "Deposito realizado"
+    responsiveVoice.speak(textDepositar.placeholder, 'Brazilian Portuguese Female', { rate: 1.1 });
   }
 }
 
