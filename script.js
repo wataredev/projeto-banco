67dc478b6701e4caa65c176433e48c605a46dd9c
Rodrigo Silva <116703859+Rodrigofms@users.noreply.github.com>
2024-09-01 17:49:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function entrar() {
   let usuario = document.querySelector("#input-iniciar").value;
 
 
-  if (senha == 3589 && usuario.length !== 0 && usuario.length <= 20 && !/[^A-Za-z\sáàâãéèêíïóôõöúçñÁÀÂÃÉÈÊÍÏÓÔÕÖÚÇÑ]/.test(usuario)) {
+  if (senha == 3589 && usuario.length !== 0 && usuario.length <= 20 && /[A-Za-z\sáàâãéèêíïóôõöúçñÁÀÂÃÉÈÊÍÏÓÔÕÖÚÇÑ]/.test(usuario)) {
     localStorage.setItem("usuario", usuario);
     window.open("banco.html", "_self");
   } else if (usuario.length === 0 || usuario.length > 20 || /[^A-Za-z\sáàâãéèêíïóôõöúçñÁÀÂÃÉÈÊÍÏÓÔÕÖÚÇÑ]/.test(usuario)) {
